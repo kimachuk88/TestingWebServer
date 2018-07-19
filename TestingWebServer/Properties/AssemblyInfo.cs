db83dcb6677b6be6e164b90e3dc088ec330e8175
Andriy <kimachuk88@gmail.com>
2018-07-19 11:10:49 +0300

    Add project files.

---
+++ TestingWebServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingWebServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TestingWebServer")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0fb86365-47cf-49df-b021-955f02eeeb72")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestingWebServer/Tools/TestRunner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingWebServer.Tools
+{
+    class TestRunner
+    {
+        ServiceReference.CommonClient common = new ServiceReference.CommonClient();
+        string token;
+
+        public string Login(string name, string password)
+        {
+            token = common.login(name, password);
+            return token;
+        }
+
+        public bool Create(string token, string name, string password)
+        {
+            return common.createUser(token, name, password, true);
+
+        }
+
+        public bool Logout(string name, string token)
+        {
+            return common.logout(name, token);
+        }
+
+        public string GetUserName(string token)
+        {
+            return common.getUserName(token);
+        }
+
+        public String GetAllUsers(string token)
+        {
+            return common.getAllUsers(token);
+        }
+
+        public bool RemoveUser(string token, string username)
+        {
+            return common.removeUser(token, username);
+        }
+    }
+}
